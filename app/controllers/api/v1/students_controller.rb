dedd0daca6791a16bd3d0597e2f9ab32fa50b311
khargr14 <kay_hargrove@yahoo.com>
2021-07-29 21:41:34 -0400

    CRUD in controllers

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -1,7 +1,41 @@
 class Api::V1::StudentsController < ApplicationController
-    def index
-        students = Student.all
-        
-        render json: students
+
+  def index
+      students = Student.all
+      
+      render json: students
+    end
+  
+    def create
+      
+      student = Student.new(student_params)
+      if student.save
+        render json: student
+      else
+        render json: {error: 'Error creating student'}
       end
+    end
+  
+    def show
+      student = Student.find(params[:id])
+      render json: student
+    end
+  
+    def update
+      student = Student.find(params[:id])
+      student.update(student_params)
+      render json: student
+    end
+  
+    def destroy
+      student = Student.find(params[:id])
+      student.destroy
+      render json: {status: :ok}
+    end
+  
+    private
+  
+    def student_params
+      params.require(:student).permit(:name, :number, :email, :teacher_id)
+    end
 end
--- app/controllers/api/v1/teachers_controller.rb
+++ app/controllers/api/v1/teachers_controller.rb
@@ -1,2 +1,49 @@
 class Api::V1::TeachersController < ApplicationController
+    #  before_action :set_teacher
+      before_action :set_student#, only: [:show, :update, :edit, :]
+
+    def index
+        teachers = @student.teachers
+        
+        render json: teachers
+      end
+    
+      def create
+        teacher = @student.teachers.new(teacher_params)
+        puts teacher.inspect
+        if teacher.save
+          render json: @student
+        else
+          puts teacher.errors.inspect
+          render json: {error: 'Error creating teacher'}
+        end
+      end
+    
+      def show
+        teacher = @student.teachers.find_by(id: params[:id])
+        render json: teacher
+      end
+    
+      def update
+        teacher = Teacher.find(params[:id])
+        teacher.update(teacher_params)
+        render json: teacher
+      end
+    
+      def destroy
+        teacher = @student.teachers.find_by(id: params[:id])
+        teacher.destroy
+        render json: @student
+      end
+    
+      private
+    
+      def set_student
+        @student = Student.find(params[:student_id])
+      end
+      
+    
+      def teacher_params
+        params.require(:teacher).permit(:student_id, :name, :email, :date, :message)
+      end
 end
