3ac8b086baaf91d2dbc7d7804b316086a1c9b4d0
khargr14 <kay_hargrove@yahoo.com>
2021-07-29 20:57:08 -0400

    create api/vi controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :teachers
-  resources :students
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :students do
+        resources :teachers
+      end
+    end
+  end
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
