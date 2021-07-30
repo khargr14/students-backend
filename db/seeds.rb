ac7c5a23e24321ee87d94fef506fe8bc86682baf
khargr14 <kay_hargrove@yahoo.com>
2021-07-29 21:23:30 -0400

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+Teacher.destroy_all
+
+student = Student.create(name: 'Kelly Jones', number: '123-456-7891', email: 'kjones@jones.COM')
+teacher = Teacher.create(name: 'Mrs.Green', email: 'green@green.com', date: DateTime.now, message: 'some message', student_id: student.id)
+
+student = Student.create(name: 'Blue Lee', number: '222-456-7891', email: 'blue@blue.COM')
+teacher = Teacher.create(name: 'Mr.JIMMY', email: 'jimmy@DER.COM', date: DateTime.now, message: 'some message', student_id: student.id)
+
+student = Student.create(name: 'Kim Gems', number: '333-456-7891', email: 'kim@DER.COM')
+teacher = Teacher.create(name: 'Mr.MICKEY', email: 'AB8C@DER.COM', date: DateTime.now, message: 'some message', student_id: student.id)
+
+
